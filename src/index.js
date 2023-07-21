a6bd0970d6acb828cc63905a6f9b7075f6f47426
va7ul <vas7ul@gmail.com>
2023-07-21 14:30:41 +0300

    prestart settings

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import './js/hero';
+import './js/header';
+import './js/popular_recipes';
+import './js/all_categories';
+import './js/card_template';
+import './js/search_filters';
+import './js/all_recipes';
+
+import './js/favorites/favorite_filter';
+import './js/favorites/favorite_recipes';
+
+import './js/modals/mobile_menu';
+import './js/modals/order_now';
+import './js/modals/rating';
+import './js/modals/recipe';
