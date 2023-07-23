b489941531c615c2b5fe8629ccb07f8425171e33
26Yuliia <124077868+26Yuliia@users.noreply.github.com>
2023-07-23 19:05:07 +0300

    Добавлено, базові стилі, модальне вікно бургер, слайдер (поки тільки вліво в право) залишилось корзину модальне вікно

---
+++ src/js/modals/mobile_menu.js
@@ -0,0 +1,28 @@
+const refs = {
+  mobileMenu: document.querySelector('.js-menu-container'),
+  openMenuBtn: document.querySelector('.js-open-menu'),
+  closeMenuBtn: document.querySelector('.js-close-menu'),
+};
+
+(() => {
+  const toggleMenu = () => {
+    const isMenuOpen =
+      refs.openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    refs.openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+    refs.mobileMenu.classList.toggle('is-open');
+    const scrollLockMethod = !isMenuOpen
+      ? 'disableBodyScroll'
+      : 'enableBodyScroll';
+    bodyScrollLock[scrollLockMethod](document.body);
+  };
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+
+  // Close the mobile menu on wider screens if the device orientation changes
+  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+    if (!e.matches) return;
+    refs.mobileMenu.classList.remove('is-open');
+    refs.openMenuBtn.setAttribute('aria-expanded', false);
+    bodyScrollLock.enableBodyScroll(document.body);
+  });
+})();
