d1774d1778b3500e59afe2e3ad707a3eb217e722
Michael <fidd.michael@gmail.com>
2023-07-23 10:16:39 +0200

    render cards function & add to favorites

--- src/js/all_recipes.js
+++ src/js/all_recipes.js
@@ -1,12 +1,7 @@
-import axios from "axios";
-import Notiflix from "notiflix";
 import { getRecipes } from "./api";
 import { createCardTemplate } from "./card_template";
 
-let recipeGalleryPage = 1;
-let maxPage = 0;
 const cardsGallery = document.querySelector('.cards-gallery');
-const URL = `https://tasty-treats-backend.p.goit.global/api/recipes?page=${recipeGalleryPage}&limit=9`;
 const favorites = [];
 let dataRecipes = [];
 fetchRecipes();
@@ -16,7 +11,6 @@ fetchRecipes();
   async function fetchRecipes() {
     try {
       dataRecipes = await getRecipes();
-      console.log(dataRecipes)
       await createCardTemplate(dataRecipes, cardsGallery);
       const recipeCard = document.querySelector('.reciepe-card')
       cardsGallery.addEventListener('change', addToFavorites);
@@ -31,42 +25,6 @@ fetchRecipes();
   }
 
 
-  // function makeRecipeItems(data) {
-  //   return data.reduce(
-  //     (markup, currentEl) => markup + createGalleryItem(currentEl),
-  //     ''
-  //   );
-  // }
-
-  // function renderGallery(markup) {
-  //   cardsGallery.insertAdjacentHTML('beforeend', markup)
-  // }
-
-//   function createGalleryItem({
-//     area,
-//     category,
-//     description,
-//     ingredients,
-//     preview,
-//     rating,
-//     title,
-//     _id,
-//   }) {
-//     return `<div style="background-image: url('${preview}')"
-//     class="reciepe-card">
-//     <h3 class="reciepe-card-title">
-//         ${title}
-//     </h3>
-//     <p class="reciepe-card-description">
-//     ${description}
-//     </p>
-//     <span class="reciepe-card-rating">${rating}</span>
-//     <span class="reciepe-card-stars">*****</span>
-//     <input type="checkbox" class="add-to-fav-btn" data-id = ${_id}>
-//     <span data-area = ${area}>
-//      </input>
-// </div>`;
-//   }
 
   function addToFavorites(event){
     const recipeId = event.target.dataset.id;
