40e0eefbc596cb0bf9b7c8f9b0e8a75f0cd6906d
va7ul <vas7ul@gmail.com>
2023-07-24 15:49:21 +0300

    bug fix favorites

---
+++ src/favourites.js
@@ -0,0 +1,14 @@
+import './js/hero';
+import './js/slick.min';
+
+import './js/favorites/favorite_filter';
+import './js/favorites/favorite_recipes';
+
+import './js/modals/mobile_menu';
+import './js/modals/order_now';
+import './js/modals/rating';
+import './js/modals/recipe';
+import './js/modals/scroll-up';
+
+import './js/api';
+import './js/templates';
