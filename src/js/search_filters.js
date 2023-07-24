84ae4ee3f0bf29cd975cde5aa11b1908cf9e0189
VorontsovaYulia <vorontsovayulia89@gmail.com>
2023-07-25 00:02:26 +0300

    clear my css

--- src/js/search_filters.js
+++ src/js/search_filters.js
@@ -57,7 +57,7 @@ function createTimeOptions() {
         timeSelect = new SlimSelect({
             select: '.js-time',
          settings: {
-                showSearch: true,
+                showSearch: false,
                 placeholderText: '0 min',
   },
      })
