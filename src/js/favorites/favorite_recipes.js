867c1a678805b17eb87c2f91666ce5a242c4e54c
NatalyaMaterinskaya <natulka-91@ukr.net>
2023-07-24 03:39:15 +0300

    Added test items, empty wrapper, updated function,html,css

--- src/js/favorites/favorite_recipes.js
+++ src/js/favorites/favorite_recipes.js
@@ -1,63 +1,112 @@
 //import
-const favoriteArr = [];
-const KEY_FAVORITE = 'favotire';
-function createMarkup(arr,list) {
-  return arr.map(({ id, name }) => {
-    return { text: `${name}`, value: `${id}` };
-  });
+const KEY_FAVORITE = 'favorite';
+function createCardTemplate(data, list) {
+  const markup = data
+    .map(
+      ({ _id, preview, title, description, rating, area, ingredients, time }) =>
+        `<li class="card-template" data-id="${_id}" data-area="${area}" data-ingredients="${ingredients}" data-time="${time}">
+    <img src="${preview}" alt="${title}" class="card-template-img" />
+    <div class="card-template-info">  <h2 class="card-template-title">${title}</h2>
+  <p class="card-template-descr">${description}</p>
+  <div class="card-template-descr">${rating}</div>
+  <button class="card-template-btn" type="button">See recipe</button>
+    <button class="favorite" type="button">Favorite</button>
+  </div>
+</li>`
+    )
+    .join('');
+
+  list.innerHTML = markup;
 }
 function createModal(product) {
-    console.log('a');
+  console.log(product);
 }
 //import
 
-//export
+// Для тестування
+import axios from 'axios';
+const favoriteArr = [];
+const id = [
+  '6462a8f74c3d0ddd288980d4',
+  '6462a8f74c3d0ddd28897fb8',
+  '6462a8f74c3d0ddd28897fbd',
+  '6462a8f74c3d0ddd28897fbb',
+  '6462a8f74c3d0ddd28897fba',
+  '6462a8f74c3d0ddd288980d4',
+  '6462a8f74c3d0ddd28897fb8',
+  '6462a8f74c3d0ddd28897fbd',
+  '6462a8f74c3d0ddd28897fbb',
+  '6462a8f74c3d0ddd28897fba',
+  '6462a8f74c3d0ddd288980d4',
+  '6462a8f74c3d0ddd28897fb8',
+  '6462a8f74c3d0ddd28897fbd',
+  '6462a8f74c3d0ddd28897fbb',
+  '6462a8f74c3d0ddd28897fba',
+];
 
-//removeFromFavorite;
+const BASE_URL = 'https://tasty-treats-backend.p.goit.global/api/';
 
-//favoriteRecipesListEl;
+async function getProduct() {
+  for (let i = 0; i < id.length; i += 1) {
+    let item = await getProductList(id[i]);
+    favoriteArr.push(item);
+  }
+  console.log(favoriteArr);
+  localStorage.setItem(KEY_FAVORITE, JSON.stringify(favoriteArr));
+}
 
-//export
+async function getProductList(id) {
+  const response = await axios.get(`${BASE_URL}recipes/${id}`);
+  const data = response.data;
+  return data;
+}
+
+getProduct();
+// Для тестування
 
 const favoriteRecipesListEl = document.querySelector('.favorite-recipes-list');
+const emptyStorageEl = document.querySelector('.empty-storage-wrapper');
+console.log(emptyStorageEl);
 
-favoriteRecipesListEl.addEventListener("click", onClick)
+const favoriteArrFromLocalStorage =
+  JSON.parse(localStorage.getItem(KEY_FAVORITE)) ?? [];
 
+if (favoriteArrFromLocalStorage.length !== 0) {
+  createCardTemplate(favoriteArrFromLocalStorage, favoriteRecipesListEl);
+} else {
+  emptyStorageEl.classList.remove('is-hidden');
+}
 
-const favorite = JSON.parse(localStorage.getItem(KEY_FAVORITE)) ?? [];
-createMarkup(favorite, favoriteRecipesListEl);
-
+favoriteRecipesListEl.addEventListener('click', onClick);
 
 function onClick(evt) {
-    //відкриття модалки
-    if (evt.target.classList.contains(".info")) {
-        const product = findProduct(evt.target);
-        createModal(product);
-    }
-    //видалення з favorites
+  //відкриття модалки
+  if (evt.target.classList.contains('card-template-btn')) {
+    const product = findProduct(evt.target);
+    createModal(product);
+  }
+  //видалення з favorites
   if (evt.target.classList.contains('favorite')) {
-      removeFromFavorite(evt.target);
-}  
+    removeFromFavorites(evt.target);
+    evt.target.closest('.card-template').remove();
+  }
 }
 
 function findProduct(elem) {
-    const productId = Number(elem.closest('.card').dataset.id);
-    return favoriteArr.find(({id}) => id === productId)
+  const productId = elem.closest('.card-template').dataset.id;
+  return favoriteArrFromLocalStorage.find(({ _id }) => _id === productId);
 }
 
-function removeFromFavorite(elem) {
-      const productId = Number(elem.closest('.card').dataset.id);
-      const removeElemIdx = favoriteArr.findIndex(
-        item => item.id === productId
-      );
-      favoriteArr.splice(removeElemIdx, 1);
-      localStorage.setItem(KEY_FAVORITE, JSON.stringify(favoriteArr));
+function removeFromFavorites(elem) {
+  const productId = elem.closest('.card-template').dataset.id;
+  const removeElemIdx = favoriteArrFromLocalStorage.findIndex(
+    item => item._id === productId
+  );
+  favoriteArrFromLocalStorage.splice(removeElemIdx, 1);
+  localStorage.setItem(
+    KEY_FAVORITE,
+    JSON.stringify(favoriteArrFromLocalStorage)
+  );
 }
 
-export { favoriteRecipesListEl, removeFromFavorite };
-
-
-// function findProductbyFilter(elem) {
-//   const productId = Number(elem.closest('.card').dataset.id);
-//   return favoriteArr.find(({ id }) => id === productId);
-// }
+export { removeFromFavorites };
