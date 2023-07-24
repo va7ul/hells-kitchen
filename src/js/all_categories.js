b29a81dea616fe0419f5497f440ab174ec46af6c
va7ul <vas7ul@gmail.com>
2023-07-24 17:21:14 +0300

    test

--- src/js/all_categories.js
+++ src/js/all_categories.js
@@ -54,3 +54,34 @@ async function onAllCategory(evt) {
 
   fetchRecipes();
 }
+
+// from all recipes.js
+
+// import { pagination } from './pagination';
+
+// const listContainer = document.querySelector('#catalog-list');
+
+// async function populateCardsWithRecipes() {
+//   try {
+//     const recipesData = await getRecipes(); // Выполняем запрос на получение данных о рецептах
+//     createCardTemplate(recipesData, listContainer); // Передаем полученные данные в функцию createCardTemplate
+//   } catch (error) {
+//     console.error('Ошибка при получении данных о рецептах:', error);
+//   }
+// }
+
+// populateCardsWithRecipes();
+
+// // Обработчик события, который будет вызываться при изменении текущей страницы пагинации
+// pagination.on('afterMove', async (event) => {
+//   const currentPage = event.page;
+//   try {
+//     // Выполняем запрос на получение данных о рецептах с учетом новой текущей страницы
+//     const recipesData = await getRecipes({ page: currentPage });
+
+//     // Обновляем карточки рецептов на странице
+//     createCardTemplate(recipesData, listContainer);
+//   } catch (error) {
+//     console.error('Ошибка при получении данных о рецептах:', error);
+//   }
+// });
