01f5d2f2d6e2aa08fa603224d7dbe9ba27e7d62e
Irulik <iryna_n@hotmail.com>
2023-07-25 11:44:07 +0200

    modal data fields

--- src/js/card_template.js
+++ src/js/card_template.js
@@ -16,7 +16,7 @@ function createCardTemplate(data, list) {
   <p class="card-template-descr">${description}</p>
   <div class="card-template-rating-and-btn">
   <div class="card-template-rating">${rating}</div>
-  <button id="pop-up-modal-toogle" class="card-template-btn" type="button" data-id="${_id}")">See recipe</button>
+  <button id="pop-up-modal-toogle" class="card-template-btn" type="button" data-id="${_id}">See recipe</button>
   </div>
   </div>
   
@@ -39,9 +39,37 @@ document.addEventListener('click', function(event) {
 
 function popUpFunction(_id) {
   const modal_popup = document.querySelector('.pop-up-recipe');
-  console.log(modal_popup)
   getRecipeById(_id).then((recipe) => {
     console.log(recipe);
+
+    let current_tags = "";
+    for (let i = 0; i < recipe.tags.length; i++) {
+      current_tags += '<a href="" class="modal_tag">#'+recipe.tags[i]+"</a>";
+    }
+
+    let current_ingredients = "";
+    for (let i = 0; i < recipe.ingredients.length; i++) {
+      current_ingredients += `<div class="modal_ingredient"><span>${recipe.ingredients[i].name}</span><span>${recipe.ingredients[i].measure}</span></div>`;
+    }
+
+    let current_rating = Math.round(recipe.rating);
+    let current_stars = "";
+    for (let i = 0; i < current_rating; i++) {
+      current_stars += `<svg xmlns="http://www.w3.org/2000/svg" width="14" height="13" viewBox="0 0 14 13" fill="none">
+        <path d="M6.04894 0.927052C6.3483 0.0057416 7.6517 0.00574088 7.95106 0.927052L8.79611 3.52786C8.92999 3.93989 9.31394 4.21885 9.74717 4.21885H12.4818C13.4505 4.21885 13.8533 5.45846 13.0696 6.02786L10.8572 7.63525C10.5067 7.8899 10.3601 8.34127 10.494 8.75329L11.339 11.3541C11.6384 12.2754 10.5839 13.0415 9.80017 12.4721L7.58779 10.8647C7.2373 10.6101 6.7627 10.6101 6.41222 10.8647L4.19983 12.4721C3.41612 13.0415 2.36164 12.2754 2.66099 11.3541L3.50604 8.75329C3.63992 8.34127 3.49326 7.8899 3.14277 7.63525L0.930391 6.02787C0.146677 5.45846 0.549452 4.21885 1.51818 4.21885H4.25283C4.68606 4.21885 5.07001 3.93989 5.20389 3.52786L6.04894 0.927052Z" fill="#EEA10C"/>
+      </svg>`
+    }
+    for (let i = 0; i < 5 - current_rating; i++) {
+      current_stars += `<svg xmlns="http://www.w3.org/2000/svg" width="14" height="13" viewBox="0 0 14 13" fill="none">
+      <path d="M6.04894 0.927052C6.3483 0.0057416 7.6517 0.00574088 7.95106 0.927052L8.79611 3.52786C8.92999 3.93989 9.31394 4.21885 9.74717 4.21885H12.4818C13.4505 4.21885 13.8533 5.45846 13.0696 6.02786L10.8572 7.63525C10.5067 7.8899 10.3601 8.34127 10.494 8.75329L11.339 11.3541C11.6384 12.2754 10.5839 13.0415 9.80017 12.4721L7.58779 10.8647C7.2373 10.6101 6.7627 10.6101 6.41222 10.8647L4.19983 12.4721C3.41612 13.0415 2.36164 12.2754 2.66099 11.3541L3.50604 8.75329C3.63992 8.34127 3.49326 7.8899 3.14277 7.63525L0.930391 6.02787C0.146677 5.45846 0.549452 4.21885 1.51818 4.21885H4.25283C4.68606 4.21885 5.07001 3.93989 5.20389 3.52786L6.04894 0.927052Z" fill="grey"/>
+    </svg>`
+    }
+
+    let current_youtube = "";
+    if(recipe.youtube){
+      current_youtube = recipe.youtube.replace('watch?v=','embed/');
+    }
+
     const transformedRecipe = `
     <div class="bg_modal"></div>
     <div class="modal">
@@ -53,59 +81,38 @@ function popUpFunction(_id) {
       </div>
   
       <div class="modal_container">
-        <h4 class="modal_title">Mozarella</h4>
-        <iframe class="modal_video" src="https://www.youtube.com/embed/tgbNymZ7vqY" referrerpolicy="no-referrer" ></iframe>
+        <h4 class="modal_title">${recipe.title}</h4>
+        <iframe class="modal_video" src="${current_youtube}" referrerpolicy="no-referrer" ></iframe>
         <div class="modal_line">
           <div class="modal_tags">
-            <a href="" class="modal_tag">#hello</a>
-            <a href="" class="modal_tag">#hello</a>
-            <a href="" class="modal_tag">#hello</a>
+            ${current_tags}
           </div>
           <div class="modal_right">
             <div class="modal_rating">
-              <span>4.5</span>
+              <span>${recipe.rating}</span>
               <div class="modal_rating_stars">
-                <svg xmlns="http://www.w3.org/2000/svg" width="14" height="13" viewBox="0 0 14 13" fill="none">
-                  <path d="M6.04894 0.927052C6.3483 0.0057416 7.6517 0.00574088 7.95106 0.927052L8.79611 3.52786C8.92999 3.93989 9.31394 4.21885 9.74717 4.21885H12.4818C13.4505 4.21885 13.8533 5.45846 13.0696 6.02786L10.8572 7.63525C10.5067 7.8899 10.3601 8.34127 10.494 8.75329L11.339 11.3541C11.6384 12.2754 10.5839 13.0415 9.80017 12.4721L7.58779 10.8647C7.2373 10.6101 6.7627 10.6101 6.41222 10.8647L4.19983 12.4721C3.41612 13.0415 2.36164 12.2754 2.66099 11.3541L3.50604 8.75329C3.63992 8.34127 3.49326 7.8899 3.14277 7.63525L0.930391 6.02787C0.146677 5.45846 0.549452 4.21885 1.51818 4.21885H4.25283C4.68606 4.21885 5.07001 3.93989 5.20389 3.52786L6.04894 0.927052Z" fill="#EEA10C"/>
-                </svg>
-                <svg xmlns="http://www.w3.org/2000/svg" width="14" height="13" viewBox="0 0 14 13" fill="none">
-                  <path d="M6.04894 0.927052C6.3483 0.0057416 7.6517 0.00574088 7.95106 0.927052L8.79611 3.52786C8.92999 3.93989 9.31394 4.21885 9.74717 4.21885H12.4818C13.4505 4.21885 13.8533 5.45846 13.0696 6.02786L10.8572 7.63525C10.5067 7.8899 10.3601 8.34127 10.494 8.75329L11.339 11.3541C11.6384 12.2754 10.5839 13.0415 9.80017 12.4721L7.58779 10.8647C7.2373 10.6101 6.7627 10.6101 6.41222 10.8647L4.19983 12.4721C3.41612 13.0415 2.36164 12.2754 2.66099 11.3541L3.50604 8.75329C3.63992 8.34127 3.49326 7.8899 3.14277 7.63525L0.930391 6.02787C0.146677 5.45846 0.549452 4.21885 1.51818 4.21885H4.25283C4.68606 4.21885 5.07001 3.93989 5.20389 3.52786L6.04894 0.927052Z" fill="#EEA10C"/>
-                </svg>
-                <svg xmlns="http://www.w3.org/2000/svg" width="14" height="13" viewBox="0 0 14 13" fill="none">
-                  <path d="M6.04894 0.927052C6.3483 0.0057416 7.6517 0.00574088 7.95106 0.927052L8.79611 3.52786C8.92999 3.93989 9.31394 4.21885 9.74717 4.21885H12.4818C13.4505 4.21885 13.8533 5.45846 13.0696 6.02786L10.8572 7.63525C10.5067 7.8899 10.3601 8.34127 10.494 8.75329L11.339 11.3541C11.6384 12.2754 10.5839 13.0415 9.80017 12.4721L7.58779 10.8647C7.2373 10.6101 6.7627 10.6101 6.41222 10.8647L4.19983 12.4721C3.41612 13.0415 2.36164 12.2754 2.66099 11.3541L3.50604 8.75329C3.63992 8.34127 3.49326 7.8899 3.14277 7.63525L0.930391 6.02787C0.146677 5.45846 0.549452 4.21885 1.51818 4.21885H4.25283C4.68606 4.21885 5.07001 3.93989 5.20389 3.52786L6.04894 0.927052Z" fill="#EEA10C"/>
-                </svg>
-                <svg xmlns="http://www.w3.org/2000/svg" width="14" height="13" viewBox="0 0 14 13" fill="none">
-                  <path d="M6.04894 0.927052C6.3483 0.0057416 7.6517 0.00574088 7.95106 0.927052L8.79611 3.52786C8.92999 3.93989 9.31394 4.21885 9.74717 4.21885H12.4818C13.4505 4.21885 13.8533 5.45846 13.0696 6.02786L10.8572 7.63525C10.5067 7.8899 10.3601 8.34127 10.494 8.75329L11.339 11.3541C11.6384 12.2754 10.5839 13.0415 9.80017 12.4721L7.58779 10.8647C7.2373 10.6101 6.7627 10.6101 6.41222 10.8647L4.19983 12.4721C3.41612 13.0415 2.36164 12.2754 2.66099 11.3541L3.50604 8.75329C3.63992 8.34127 3.49326 7.8899 3.14277 7.63525L0.930391 6.02787C0.146677 5.45846 0.549452 4.21885 1.51818 4.21885H4.25283C4.68606 4.21885 5.07001 3.93989 5.20389 3.52786L6.04894 0.927052Z" fill="#EEA10C"/>
-                </svg>
-                <svg xmlns="http://www.w3.org/2000/svg" width="14" height="13" viewBox="0 0 14 13" fill="none">
-                  <path d="M6.04894 0.927052C6.3483 0.0057416 7.6517 0.00574088 7.95106 0.927052L8.79611 3.52786C8.92999 3.93989 9.31394 4.21885 9.74717 4.21885H12.4818C13.4505 4.21885 13.8533 5.45846 13.0696 6.02786L10.8572 7.63525C10.5067 7.8899 10.3601 8.34127 10.494 8.75329L11.339 11.3541C11.6384 12.2754 10.5839 13.0415 9.80017 12.4721L7.58779 10.8647C7.2373 10.6101 6.7627 10.6101 6.41222 10.8647L4.19983 12.4721C3.41612 13.0415 2.36164 12.2754 2.66099 11.3541L3.50604 8.75329C3.63992 8.34127 3.49326 7.8899 3.14277 7.63525L0.930391 6.02787C0.146677 5.45846 0.549452 4.21885 1.51818 4.21885H4.25283C4.68606 4.21885 5.07001 3.93989 5.20389 3.52786L6.04894 0.927052Z" fill="grey"/>
-                </svg>
+                ${current_stars}
               </div>
             </div>
-            <div class="modal_time">40 min</div>
+            <div class="modal_time">${recipe.time}</div>
           </div>
         </div>
   
         <div class="modal_ingredients">
-          <div class="modal_ingredient"><span>banana</span><span>1 large</span></div>
-          <div class="modal_ingredient"><span>banana</span><span>1 large</span></div>
-          <div class="modal_ingredient"><span>banana</span><span>1 large</span></div>
-          <div class="modal_ingredient"><span>banana</span><span>1 large</span></div>
-          <div class="modal_ingredient"><span>banana</span><span>1 large</span></div>
-          <div class="modal_ingredient"><span>banana</span><span>1 large</span></div>
+          ${current_ingredients}
         </div>
-        <div class="modal_description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, sequi voluptates adipisci perspiciatis, ad expedita vitae. Repellendus ex fuga quia, nobis quas odio tempore ab aliquid. Sit eius, molestias necessitatibus!</div>
+        <div class="modal_description">${recipe.description}</div>
         <div class="modal_buttons">
           <div class="modal_button modal_favourite">
-            <button>Add to favourite</button>
+            <button data-id="${recipe._id}">Add to favourite</button>
           </div>
           <div class="modal_button modal_rating_button">
-            <button>Give a rating</button>
+            <button data-id="${recipe._id}">Give a rating</button>
           </div>
         </div>
       </div>
     </div>
-    `).join('');
+    `;
     modal_popup.innerHTML = transformedRecipe;
   })
 }
