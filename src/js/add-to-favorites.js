b0c42591946c0c4c7116b08db866d8d68e787f05
Michael <fidd.michael@gmail.com>
2023-07-26 20:20:25 +0200

    pagintaion + edits in css

--- src/js/add-to-favorites.js
+++ src/js/add-to-favorites.js
@@ -7,17 +7,14 @@ const KEY_FAVORITE = 'favorite';
 
 function addToFavorites(event){
     const recipeId = event.target.closest('.card-template').dataset.id;
-    const favoriteRecipe = findRecipe(recipeId);
+    if (favoritesArray.find(({_id}) => _id === recipeId) === undefined){
+      const item = dataRecipes.find(({ _id }) => _id === recipeId)
+      favoritesArray.push(item)
+      save(KEY_FAVORITE, favoritesArray)
+     }
+     else{
+      removeFromFavorites(KEY_FAVORITE, event.target, favoritesArray);
+     }
   }
 
-function findRecipe(recipeId){
-   const item = dataRecipes.find(({ _id }) => _id === recipeId)
-   if (favoritesArray.find(({_id}) => _id === recipeId) === undefined){
-    favoritesArray.push(item)
-    save(KEY_FAVORITE, favoritesArray)
-   }
-   else{
-    removeFromFavorites(KEY_FAVORITE, item, favoritesArray);
-   }
-}
 export {addToFavorites}
