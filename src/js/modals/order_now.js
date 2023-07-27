e9326e7c370e5986e9a06f602742cca2363d67a1
va7ul <vas7ul@gmail.com>
2023-07-28 01:16:31 +0300

    дописав бекенд для modal order now

--- src/js/modals/order_now.js
+++ src/js/modals/order_now.js
@@ -1,3 +1,8 @@
+import { postOrder } from '../api';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { Loading } from 'notiflix';
+import { disablePageScroll, enablePageScroll } from 'scroll-lock';
+
 const refs = {
   modalOrderNowForm: document.querySelector('.order-now-modal-form'),
   openButtonEl: document.querySelector('.shopping-cart'),
@@ -35,7 +40,7 @@ const refs = {
 //}
 //export { onModalOpen };
 
-console.log(refs.modalOrderNowForm.elements);
+// console.log(refs.modalOrderNowForm.elements);
 
 if (refs.openButtonHeroEl) {
   refs.openButtonHeroEl.addEventListener('click', onModalOpen);
@@ -50,6 +55,7 @@ refs.backdropEl.addEventListener('click', onBackdropClick);
 refs.modalOrderNowForm.addEventListener('submit', onSubmitForm);
 
 function onModalOpen() {
+  disablePageScroll();
   window.addEventListener('keydown', onEscKeyPress);
   document.body.classList.add('show-modal-order-now');
 }
@@ -57,6 +63,7 @@ function onModalOpen() {
 function onModalRemove() {
   window.removeEventListener('keydown', onEscKeyPress);
   document.body.classList.remove('show-modal-order-now');
+  enablePageScroll();
 }
 
 function onBackdropClick(event) {
@@ -73,12 +80,12 @@ function onEscKeyPress(event) {
 
 function extractFormData(form) {
   let formData = {};
-  
+
   formData.name = form.querySelector('input[name="name"]').value;
   formData.phone = form.querySelector('input[name="phone"]').value;
   formData.email = form.querySelector('input[name="email"]').value;
   formData.comment = form.querySelector('textarea[name="comment"]').value;
-  
+
   return formData;
 }
 
@@ -86,43 +93,27 @@ function onSubmitForm(e) {
   e.preventDefault();
 
   const formData = extractFormData(refs.modalOrderNowForm);
-  console.log(formData);
 
+  const { name, phone, email, comment } = formData;
 
-
-
-
-
-
-  refs.modalOrderNowForm.reset();
-}
-
-
-
-function submitRating(evt) {
-  evt.preventDefault();
-  let giveRating = Number(refs.starsEl.textContent);
-  let email = refs.inputEl.value;
-  // треба підключити ID з картки
-  let recipeId = '6462a8f74c3d0ddd288980d4';
   const options = {
-    rate: giveRating,
+    name,
+    phone,
     email,
+    comment,
   };
 
-  patchRating(recipeId, options)
+  postOrder(options)
     .then(categories => {
       // console.log(categories);
-      if (localStorage.getItem('patch-rating') == 'error') {
-        return Notify.failure('Oops! Something went wrong!');
+      if (localStorage.getItem('patch-rating') !== 'error') {
+        onModalRemove();
+        Notify.success('Thank you for your order!');
       }
-      onRatingModalRemove();
-      refs.starsEl.textContent = '0.0';
-      refs.inputEl.value = '';
-      Notify.success('Thank you for your feedback!');
+      return;
     })
     .catch(error => console.log(error))
     .finally(() => Loading.remove());
+
+  // refs.modalOrderNowForm.reset()
 }
-
-
