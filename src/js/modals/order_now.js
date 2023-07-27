d1017be605e3dd98cc359222419994c12659ea6b
26Yuliia <124077868+26Yuliia@users.noreply.github.com>
2023-07-28 01:06:10 +0300

    header
    1. заміна падінгів
    2. не скролиться мобільне меню
    3. не скролиться коли відкривається модалка order now
    4. стилі на модалку підкореговані

--- src/js/modals/order_now.js
+++ src/js/modals/order_now.js
@@ -1,3 +1,4 @@
+import { disablePageScroll, enablePageScroll } from 'scroll-lock';
 const refs = {
   modalOrderNowForm: document.querySelector('.order-now-modal-form'),
   openButtonEl: document.querySelector('.shopping-cart'),
@@ -5,37 +6,6 @@ const refs = {
   closeButtonEl: document.querySelector('.order-now-modal__close-btn'),
   backdropEl: document.querySelector('.js-backdrop'),
 };
-
-///refs.openButtonEl.addEventListener('click', onModalOpen);
-
-//refs.closeButtonEl.addEventListener('click', onModalRemove);
-
-//refs.backdropEl.addEventListener('click', onBackdropClick);
-
-//function onModalOpen() {
-//window.addEventListener('keydown', onEscKeyPress);
-// document.body.classList.add('show-modal-order-now');
-//}
-
-//function onModalRemove() {
-////  window.removeEventListener('keydown', onEscKeyPress);
-////  document.body.classList.remove('show-modal-order-now');
-/////}
-
-//function onBackdropClick(event) {
-// if (event.currentTarget === event.target) {
-//   onModalRemove();
-// }
-//}
-
-//function onEscKeyPress(event) {
-// if (event.code === 'Escape') {
-//   onModalRemove();
-//// }
-//}
-//export { onModalOpen };
-
-console.log(refs.modalOrderNowForm.elements);
 
 if (refs.openButtonHeroEl) {
   refs.openButtonHeroEl.addEventListener('click', onModalOpen);
@@ -50,6 +20,7 @@ refs.backdropEl.addEventListener('click', onBackdropClick);
 refs.modalOrderNowForm.addEventListener('submit', onSubmitForm);
 
 function onModalOpen() {
+  disablePageScroll();
   window.addEventListener('keydown', onEscKeyPress);
   document.body.classList.add('show-modal-order-now');
 }
@@ -57,6 +28,7 @@ function onModalOpen() {
 function onModalRemove() {
   window.removeEventListener('keydown', onEscKeyPress);
   document.body.classList.remove('show-modal-order-now');
+  enablePageScroll();
 }
 
 function onBackdropClick(event) {
@@ -73,12 +45,12 @@ function onEscKeyPress(event) {
 
 function extractFormData(form) {
   let formData = {};
-  
+
   formData.name = form.querySelector('input[name="name"]').value;
   formData.phone = form.querySelector('input[name="phone"]').value;
   formData.email = form.querySelector('input[name="email"]').value;
   formData.comment = form.querySelector('textarea[name="comment"]').value;
-  
+
   return formData;
 }
 
@@ -88,16 +60,8 @@ function onSubmitForm(e) {
   const formData = extractFormData(refs.modalOrderNowForm);
   console.log(formData);
 
-
-
-
-
-
-
   refs.modalOrderNowForm.reset();
 }
-
-
 
 function submitRating(evt) {
   evt.preventDefault();
@@ -124,5 +88,3 @@ function submitRating(evt) {
     .catch(error => console.log(error))
     .finally(() => Loading.remove());
 }
-
-
