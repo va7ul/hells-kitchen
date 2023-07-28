2dd770126b2ee13ff59b11846083dad58d4500c9
va7ul <vas7ul@gmail.com>
2023-07-28 06:20:41 +0300

    fix

--- src/js/add-to-favorites.js
+++ src/js/add-to-favorites.js
@@ -1,35 +1,37 @@
-import { dataRecipes } from "./all_recipes";
-import { save } from "./api";
-import { removeFromFavorites } from "./api";
+import { dataRecipes } from './all_recipes';
+import { save } from './api';
+import { removeFromFavorites } from './api';
 
 const KEY_FAVORITE = 'favorite';
 const favoritesArray = JSON.parse(localStorage.getItem(KEY_FAVORITE)) ?? [];
 
-function addToFavorites(event){
-    const recipeId = event.target.closest('.card-template').dataset.id;
-    if (favoritesArray.find(({_id}) => _id === recipeId) === undefined){
-      const item = dataRecipes.find(({ _id }) => _id === recipeId)
-      favoritesArray.push(item)
-      save(KEY_FAVORITE, favoritesArray)
-     }
-     else{
-      removeFromFavorites(KEY_FAVORITE, event.target, favoritesArray);
-      
-     }
+function addToFavorites(event) {
+  const recipeId = event.target.closest('.card-template').dataset.id;
+  if (favoritesArray.find(({ _id }) => _id === recipeId) === undefined) {
+    const item = dataRecipes.find(({ _id }) => _id === recipeId);
+    favoritesArray.push(item);
+    save(KEY_FAVORITE, favoritesArray);
+  } else {
+    removeFromFavorites(KEY_FAVORITE, event.target, favoritesArray);
   }
+}
 
-  async function isInFavorites (recipeArray, favoritesArray){
-    for (let i = 0; i < favoritesArray.length; i+= 1){
-      const recipeFav = recipeArray.find(({ _id }) => _id === favoritesArray[i]._id)
-      if (recipeFav === undefined){
-        return
-      }
-        if(recipeFav._id === favoritesArray[i]._id){
-      const recipeCardEl = document.querySelector(`.card-template[data-id="${recipeFav._id}"]`)
+async function isInFavorites(recipeArray, favoritesArray) {
+  for (let i = 0; i < favoritesArray.length; i += 1) {
+    const recipeFav = recipeArray.find(
+      ({ _id }) => _id === favoritesArray[i]._id
+    );
+    if (recipeFav === undefined || null) {
+      return;
+    }
+    if (recipeFav._id === favoritesArray[i]._id) {
+      const recipeCardEl = document.querySelector(
+        `.card-template[data-id="${recipeFav._id}"]`
+      );
       recipeCardEl.firstElementChild.firstElementChild.checked = true;
-      } else{
-      return   
-      }
+    } else {
+      return;
+    }
   }
-  }
-export {addToFavorites, isInFavorites, favoritesArray}
+}
+export { addToFavorites, isInFavorites, favoritesArray };
