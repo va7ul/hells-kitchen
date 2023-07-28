825551cee333537d9d243e173dbc0e0ff69892fc
va7ul <vas7ul@gmail.com>
2023-07-28 15:43:39 +0300

    bug fix

--- src/js/all_recipes.js
+++ src/js/all_recipes.js
@@ -1,36 +1,43 @@
-import { getRecipes } from './api';
-import { createCardTemplate } from './card_template';
-import {
-  addToFavorites,
-  isInFavorites,
-  favoritesArray,
-} from './add-to-favorites';
-import { pagination } from './pagination';
-import Notiflix from 'notiflix';
+import { getRecipes} from "./api";
+import { createCardTemplate } from "./card_template";
+import { addToFavorites, isInFavorites, favoritesArray } from "./add-to-favorites";
+import { pagination } from "./pagination";
+import Notiflix from "notiflix";
 
 const cardsGallery = document.querySelector('.cards-gallery');
 let dataRecipes = [];
 
-fetchRecipes();
+fetchRecipes();  
 
-async function fetchRecipes() {
-  try {
-    dataRecipes = await getRecipes({ page: 1 });
-    const recipeCard = document.querySelector('.card-template');
-    createCardTemplate(dataRecipes, cardsGallery);
-    isInFavorites(dataRecipes, favoritesArray);
-    cardsGallery.addEventListener('change', addToFavorites);
-    return dataRecipes;
-  } catch (error) {
-    onError(error);
+  async function fetchRecipes() {
+    try {
+      dataRecipes = await getRecipes({ page: 1 });      
+      const recipeCard = document.querySelector('.card-template');
+      createCardTemplate(dataRecipes, cardsGallery);
+      isInFavorites(dataRecipes, favoritesArray )
+      cardsGallery.addEventListener('change', addToFavorites);
+      return dataRecipes;
+    } catch (error) {
+      onError(error);
+    }
+    console.log(dataRecipes)
   }
-  console.log(dataRecipes);
-}
 
-function onError(error) {
-  Notiflix.Notify.failure(error.message);
-}
+  function onError(error) {
+    Notiflix.Notify.failure(error.message);
+  }
 
+  // pagination.on('afterMove', async (event) => {
+  //     const currentPage = event.page;
+  //     localStorage.setItem('page', currentPage)
+  //     try {
+  //       const dataRecipes= await getRecipes({ page: currentPage });
+  //       createCardTemplate(dataRecipes, cardsGallery);
+  //     } catch (error) {
+  //       console.error('Ошибка при получении данных о рецептах:', error);
+  //     }});
 
+ 
 
-export { fetchRecipes, dataRecipes };
+export {fetchRecipes, dataRecipes}
+
